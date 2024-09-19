fd957250aeabc125a16bfea6033cadccb6c6d966
chanmanch <chamanchanchlani@gmail.com>
2024-09-19 09:30:51 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("hff","jg")
+        ERC721("efu","hf")
         Ownable(initialOwner)
     {}
 
